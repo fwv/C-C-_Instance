6d615037fb6dc28d31804055d62b5ce0cf7bc41e
fwv <cqufengwei@icloud.com>
2019-07-14 23:50:15 +0800

    update:p1003

--- pat/p1003.cpp
+++ pat/p1003.cpp
@@ -31,12 +31,12 @@ void p1003() {
     scyNum = (int*)malloc(sizeof(int)*cityNUm);
     d = (int*)malloc(sizeof(int)*cityNUm);
     maxScyNum = (int*)malloc(sizeof(int)*cityNUm);
-    adj = (vector<city>*)malloc(sizeof(vector<city>)*cityNUm);
+    adj = (vector<city>*)malloc(sizeof(vector<city>)*cityNUm); 
     visited = (bool*)malloc(sizeof(bool)*cityNUm);
 
     for(int i = 0; i < cityNUm; i++) {
             d[i] = INF;     //初始化单源最短距离数组
-            maxScyNum[i] = -1;   //初始化单源最大搜救队数量数组
+            maxScyNum[i] = -1;   //初始化单源最快最多搜救队数量数组
             city c;         //邻接表添加头结点
             c.v = i;
             c.w = 0;
@@ -57,6 +57,7 @@ void p1003() {
 
     //计算最小路径数量 思路：djiskal单源扫描，遇到目标节点距离d[end]每比较一次记录一次，自己节点不记录
     d[startCity] = 0;
+    maxScyNum[startCity] = scyNum[startCity];
     //循环N次，构建MST
     for(int i = 0; i < cityNUm; i++) {
         //查找相邻最近节点
@@ -72,9 +73,25 @@ void p1003() {
         vector<city> adjCitys = adj[newCity];
         int size = adjCitys.size();
         for(city c : adjCitys) {
+            //跳过头节点
+            if (c.v == newCity || visited[c.v]) {continue;}
+
             int newDistance = d[newCity] + c.w;
-            if(newDistance < d[c.v]) { //这里不判断是否访问过
+            int newScyNum = maxScyNum[newCity] + scyNum[c.v];
+            if(newDistance <= d[c.v]) { //这里不判断是否访问过
+                if(newDistance == d[c.v]) {
+                    if(newScyNum > maxScyNum[c.v]) {
+                        maxScyNum[c.v] = newScyNum;
+                    }
+                } else {
+                        maxScyNum[c.v] = newScyNum;
+                }
                 d[c.v] = newDistance;
+                
+            } else {
+                if (-1 == maxScyNum[c.v]) {
+                      maxScyNum[c.v] = newScyNum;  
+                }
             }
             //判断比较节点是不是目标节点，且不是自身
             if(endCity == c.v && c.w != 0) {
@@ -96,36 +113,7 @@ void p1003() {
            minDisCnt++;
         }
     }
-
-    //重置状态
-    for(int i = 0; i < cityNUm; i++) {
-        visited[i] = false;
-        maxScyNum[i] = -1;
-    }
-
-    //计算最大搜救队集结数量（思路：djiskal单源扫描，以城市搜救队数量为权重，取大）
-    maxScyNum[startCity] = scyNum[startCity];
-    //循环n次 prim构建MST
-    for(int i = 0; i < cityNUm; i++) {
-        //查找权重最大的邻接未访问节点
-        int c = -1; 
-        int maxScyCnt = 0;
-        for (int i = 0; i < cityNUm; i++)
-        {
-            if(!visited[i] && maxScyNum[i] > maxScyCnt) {
-                maxScyCnt = scyNum[i];
-                c = i;
-            }
-        }
-        //更新新节点的集结数信息
-        vector<city> citys = adj[c];
-        for(city adj_c : citys) {
-            if(adj_c.v != c && scyNum[adj_c.v] + maxScyNum[c] > maxScyNum[adj_c.v]) {
-                 maxScyNum[adj_c.v] = scyNum[adj_c.v] + maxScyNum[c];
-            } 
-        }
-        visited[c] = true;
-    }
+  
     int toEndMaxScyNum = maxScyNum[endCity];
     // cout << minDisCnt+" "+toEndMaxScyNum << endl; //不理解为什么输出奇怪的字符
     std::printf("%d %d", minDisCnt, toEndMaxScyNum);
@@ -136,6 +124,11 @@ void addEdge(vector<city>* adj, int start, int end, int w) {
     c.v = end;
     c.w = w;
     adj[start].push_back(c);
+
+    city c1;
+    c1.v = start;
+    c1.w = w;
+    adj[end].push_back(c1);
 }
 
 void showAdj(vector<city>* adj, int len) {
