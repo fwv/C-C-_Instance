b6aa375024e1dc67b3ee867cad014129ddda2f69
fwv <cqufengwei@icloud.com>
2020-06-23 15:47:02 +0800

    [add] add lt template class

---
+++ leetcode/lt_temp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+#ifndef _BASESMOOTHINGALGORITHM_H_
+#define _BASESMOOTHINGALGORITHM_H_
+#include "lt.cpp"
+#endif
+
+using namespace std;
+
+class lt_temp : lt
+{
+    public:
+    
+        void run() override
+        {
+
+        }
+};
