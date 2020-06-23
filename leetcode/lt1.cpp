eb6de88ba786326d569ca406220255e1ced77b06
fwv <cqufengwei@icloud.com>
2020-06-23 13:17:01 +0800

    [add] leetcode 1 answer

---
+++ leetcode/lt1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+
+#ifndef _BASESMOOTHINGALGORITHM_H_
+#define _BASESMOOTHINGALGORITHM_H_
+#include "lt.cpp"
+#endif
+
+using namespace std;
+class lt1 : lt
+{
+public:
+    //1.hash table
+    map<int,int> hash_t;
+    int index = 0;
+    vector<int> rlt;
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int>::iterator it = nums.begin();
+       for (; it != nums.end(); it++)
+       {
+           int temp = target - *(it);
+           if (hash_t.end() != hash_t.find(temp))
+           {
+               rlt.push_back(hash_t[temp]);
+               rlt.push_back(index);
+               return rlt;
+           }
+           hash_t[*(it)] = index++;
+       }
+       return rlt;
+    }
+
+    void run() override
+    {
+    }
+};
